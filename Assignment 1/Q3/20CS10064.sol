51ae8fdd68c93869f8c865161f057a9e1bc42ec4
Subhajyoti Halder <75578800+subhu-darkknight72@users.noreply.github.com>
2023-09-13 20:00:27 +0530

    Q# Contract written. to be deployed

--- Assignment 1/Q3/20CS10064.sol
+++ Assignment 1/Q3/20CS10064.sol
@@ -1,31 +1,83 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract TicketBooking {
-    constructor(uint _quota, uint _price) {
-       owner = msg.sender;
-       numTicketsSold = 0;
-       quota = _quota;
-       price = _price;
-    }
+// Group Members:
+//      20CS10064 - Subhajyoti Halder
+//      20CS30019 - Gitanjali Gupta
+//      20CS30023 - Hardik Pravin Soni
+//      20CS10047 - Priyanshi Dixit
 
-    function buyTicket(string memory email, uint numTickets) public payable soldOut {
-        ...
+contract TicketBooking {
+    struct Buyer {
+        uint totalPrice;
+        uint numTickets;
+        string email;
+    }
+    
+    address public owner;
+    uint public numTicketsSold;
+    uint public maxOccupancy;
+    uint public price;
+
+    mapping(address => Buyer) public buyersPaid;
+
+    constructor(uint _quota, uint _price) {
+        owner = msg.sender;
+        numTicketsSold = 0;
+        // quota = _quota;
+        maxOccupancy = _quota;
+        price = _price;
     }
 
     modifier soldOut() {
-       require(numTicketsSold < quota, "All tickets have beensold"); _;
+       require(numTicketsSold < maxOccupancy, "All tickets have been sold"); _;
     }
 
     modifier onlyOwner() { 
-        ...
+        require(msg.sender == owner, "Only the owner can call this function"); _;
     }
 
-    function withdrawFunds() public onlyOwner { 
-        ...
+    function buyTicket(string memory email, uint numTickets) public payable soldOut {
+        require(numTickets > 0, "Number of tickets must be greater than 0");
+        require(numTicketsSold + numTickets <= maxOccupancy, "Not enough tickets available");
+
+        Buyer storage buyer = buyersPaid[msg.sender];
+
+        if (buyer.numTickets == 0) {
+            buyer.email = email;
+        }
+
+        buyer.numTickets += numTickets;
+        buyer.totalPrice += msg.value;
+
+        numTicketsSold += numTickets;
+
+        // Refund excess payment
+        if (msg.value > (numTickets * price)) {
+            payable(msg.sender).transfer(msg.value - (numTickets * price));
+        }
     }
 
     function refundTicket(address buyer) public onlyOwner { 
-        ...
+        Buyer storage B = buyersPaid[buyer];
+        require(B.numTickets > 0, "Buyer has not purchased any tickets");
+
+        uint refundAmount = B.totalPrice;
+        B.totalPrice = 0;
+        B.numTickets = 0;
+
+        payable(buyer).transfer(refundAmount);
+    }
+
+    function withdrawFunds() public onlyOwner {
+        payable(owner).transfer(address(this).balance);
+    }
+
+    function getBuyerAmountPaid(address buyer) public view returns (uint) {
+        return buyersPaid[buyer].totalPrice;
+    }
+
+    function kill() public onlyOwner {
+        selfdestruct(payable(owner));
     }
 }
