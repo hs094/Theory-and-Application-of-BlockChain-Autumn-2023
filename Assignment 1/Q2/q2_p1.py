920a58b76a68b4e57dfd5a47e8f962dec50f2e1f
Subhajyoti Halder <75578800+subhu-darkknight72@users.noreply.github.com>
2023-09-15 21:34:29 +0530

    Q3 updated and tested

--- Assignment 1/Q2/q2_p1.py
+++ Assignment 1/Q2/q2_p1.py
@@ -7,7 +7,7 @@ contract_addr = "0xF98bFe8bf2FfFAa32652fF8823Bba6714c79eDd4"
 
 contract_instance = w3.eth.contract(abi=abi, address=contract_addr)
 
-# q_addr = "0x328Ff6652cc4E79f69B165fC570e3A0F468fc903"
-q_addr = "0x9b3710f9b284eAC355F1721B00C381c626250439"
+q_addr = "0x328Ff6652cc4E79f69B165fC570e3A0F468fc903"
+# q_addr = "0x9b3710f9b284eAC355F1721B00C381c626250439"
 roll = contract_instance.functions.get(q_addr).call()
 print(f"Roll: {roll}")
